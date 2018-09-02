efdfbef711854341fef3390d43f49980c2c3d81f
lucasluizss <lucasluizss@live.com>
2018-09-02 20:12:01 -0300

    Adicionando últimos exercícios feitos.

--- modulo1/exercicios/Exercicios.py
+++ modulo1/exercicios/Exercicios.py
@@ -1,15 +1,19 @@
 import operator
 from  lib.scrapy_table import Scrapy_Table
-
 url="https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_do_Brasil_por_popula%C3%A7%C3%A3o"
 
-url1 = "https://pt.wikipedia.org/wiki/Lista_de_unidades_federativas_do_Brasil_por_taxa_de_fecundidade" 
+url1 = "https://pt.wikipedia.org/wiki/Lista_de_unidades_federativas_do_Brasil_por_taxa_de_fecundidade"
+
+url2 = "https://pt.wikipedia.org/wiki/Lista_de_cidades_por_taxa_de_homic%C3%ADdios"
 
 site_connect = Scrapy_Table(url)
 tables = site_connect.get_tables(0)
 
 site_connect1 = Scrapy_Table(url1)
 tables1 = site_connect1.get_tables(1)
+
+site_connect2 = Scrapy_Table(url2)
+tables2 = site_connect2.get_tables(1)
 
 #1. Listar o nome de todos os municípios do Brasil com o código do IBGE: Exemplo de Resposta:
 def exercise1():
@@ -21,11 +25,11 @@ def exercise1():
 #2. Listar os municípios com menos de 70.000 habitantes:
 def exercise2():
   print('N IBGE  Municipio\n--------------------')
-
-  for linha in tables[1:]:
-    if int(linha[4]) < 70000:
-      print('{0} {1}'.format(linha[1], linha[2]))
-
+  list_municipios = list(filter(lambda x : int(x[4]) < 70000, tables[1:]))
+  
+  for linha in list_municipios:
+    print('{0} {1}'.format(linha[1], linha[2]))
+  
 #3. Mostrar o estado que tem o maior número de cidades acima 100.000 habitantes:
 def exercise3():  
   states = {}
@@ -74,22 +78,47 @@ def exercise5():
 
 #6. Mostrar o total da população brasileira:
 def exercise6():
-  print('Não implementado...')
-
+  print('População brasileira: ', sum(map(lambda x: int(x[4]), tables[1:])))
+  
 #7. Quantos estados brasileiros estão abaixo da taxa de reposição populacional (2.10 filhos):
-def exercise7():
-  print('Não implementado...')
+def exercise7():  
+  beginTax = [x for x in tables1[1:] if x[1] == 'Taxa de reposição populacional'][0][2]  
+  print('Estados brasileiros abaixo da taxa de reposição: ', 
+        len(list(filter(lambda x: x[2] < beginTax, tables1[1:]))))
 
 #8. Quantos habitantes moram no sudeste (São Paulo, Minas Gerais, Rio de Janeiro, Espírito Santo):
 def exercise8():
-  print('Não implementado...')
+  sudeste = ['São Paulo', 'Minas Gerais', 'Rio de Janeiro', 'Espírito Santo']
+  habitantes_sudeste = list(filter(lambda x: x[3] in sudeste, tables[1:]))
+  
+  print('Habitantes no sudeste: ', sum(map(lambda x: int(x[4]), habitantes_sudeste)))
 
 #9. Mostrar o nome, número de habitantes e taxa de fecundidade para as cidades brasileiras que estão 
 #com taxa de homicídio acima de 60 por 100 Mil:
 def exercise9():
-  print('Não implementado...')
+  filtro_homicidio = list(filter(lambda x: float(x[5].replace(',','.')) > 60 and x[2] == 'Brasil', tables2[1:]))
+  filtro_fecundidade = {}
+  filtro_municipio = {}
+  
+  # município - estado
+  for item in tables[1:]:
+    filtro_municipio[str(item[2])] = item[3]
 
+  # estado - fecundidade
+  for item in tables1[1:]:
+    filtro_fecundidade[item[1]] = item[2]
+
+  for item in filtro_homicidio:
+    if str(item[1]) in filtro_municipio:
+      taxa_fecundidade = filtro_fecundidade[filtro_municipio[str(item[1])]]
+
+    print('\nNome: {0}\nNúmero de Habitantes: {1}\nTaxa de fecundidade: {2}'.format(item[1], item[4], taxa_fecundidade))
+  
 #10. Quantos habitantes brasileiros estão morando nas 50 cidades mais violentas do mundo:
 def exercise10():
-  print('Não implementado...')
+  br_list = list(filter(lambda x: x[2] == 'Brasil', tables2[1:]))
+
+  sum_br = sum(map(lambda x: int(x[4].replace(' ','')), br_list))
+
+  print(sum_br, ' Brasileiros.')
 
